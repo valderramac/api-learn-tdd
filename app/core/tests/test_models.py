1f56eb3b9c131e8a10b3017fe3a29d919124ff13
Christian Valderrama <valderrama@Christians-MacBook-Pro.local>
2020-04-05 01:20:33 -0600

    updated admin to suport custome user module

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTest(TestCase):
     def test_create_user_with_email_succesful(self):
         """Test creating a new user with an email is succesful"""
