fed4e1cf386242a63ead57eadf950c5b6bf23085
Christian Valderrama <valderrama@Christians-MacBook-Pro.local>
2020-04-12 01:29:03 -0500

    Added image field to recipe model.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -51,4 +51,4 @@ class AuthTokenSerializer(serializers.Serializer):
             raise serializers.ValidationError(msg, code='authentication')
 
         attrs['user'] = user
-        return attrs
+        return attrs        
